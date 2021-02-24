6242fdf3781aeae1e3825060f1c1ebd4fb942eca
mubarak23 <mubarakaminu340@gmail.com>
2021-02-24 16:41:21 +0100

    change role table name

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -12,7 +12,7 @@ class RoleSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('users')->insert([
+        DB::table('role')->insert([
             'name' => 'admin'
         ],
         [
